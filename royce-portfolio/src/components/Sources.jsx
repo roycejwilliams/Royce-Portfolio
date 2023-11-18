31df2ba07d3cf21c0a48433b08b8f3ed58e0b3ac
roycejwilliams <roycewilliamsj@gmail.com>
2023-11-18 13:14:19 -0600

    final update

--- royce-portfolio/src/components/Sources.jsx
+++ royce-portfolio/src/components/Sources.jsx
@@ -8,7 +8,7 @@ const Sources = () => {
         {/* Biography */}
         <Link to="/bio" className='md:w-48 md:h-72 w-32 h-52 rounded-md m-4 cursor-pointer shadow-lg shadow-gray-300 bg-cover bg-center hover:shadow-lg hover:shadow-white transition ease-in duration-800 bg-gradient-to-b from-black/50 via-black/25 to-black/50 '>
           <h1 className='m-4 absolute text-md text-white font-clash'>Bio</h1>
-          <img className='w-full h-full ounded-md mix-blend-soft-light' src="./src/images/bio.jpeg" alt="bio" />
+          <img className='w-full h-full rounded-md mix-blend-soft-light' src="./src/images/bio.jpeg" alt="bio" />
         </Link>
         {/* Inspiration */}
         <Link to="/inspo" className='md:w-48 md:h-72 w-32 h-52 rounded-md m-4 cursor-pointer shadow-lg shadow-gray-300 bg-cover bg-center hover:shadow-lg hover:shadow-white transition ease-in duration-800 bg-gradient-to-b from-black/50 via-black/25 to-black/50' 
